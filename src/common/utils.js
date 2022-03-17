d7e8ccdc893f207c539ac29fdb4e9f884bb115b0
xavia9 <xavia_1@163.com>
2022-03-17 21:51:48 +0800

    下载文件工具函数

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,3 +1,8 @@
+/**
+ * 获取地址栏参数，转换成对象
+ *@param {url} String
+ *@return {}
+ */
 export function getQueryObject(url) {
   url = url == null ? window.location.href : url
   const search = url.substring(url.lastIndexOf('?') + 1)
@@ -15,4 +20,36 @@ export function getQueryObject(url) {
   return obj
 }
 
-export function downloadFile() {}
+/**
+ * 下载文件，并设置文件名
+ *@param {url} String
+ *@param {params} Object
+ *@return {}
+ */
+export function downloadFile(url, params) {
+  // const params = this.getParams()
+  this.fileRequest({
+    // url: '/exportPythonFile',
+    url,
+    method: 'post',
+    data: params,
+    responseType: 'blob',
+  }).then((res) => {
+    // 1.生成文件下载链接
+    let binaryData = []
+    binaryData.push(res)
+    // const url = window.URL.createObjectURL(res);
+    let url = window.URL.createObjectURL(new Blob(binaryData))
+    const a = document.createElement('a')
+    a.style.display = 'none'
+    a.href = url
+    // 2.获取文件名
+    let disposition = res.headers['content-disposition']
+    let fileName = disposition.split('attachment;filename=')[1]
+    a.download = fileName // 文件名称
+    // a.download = 'ruleCollections.py' // 文件名称
+    // 下载文件
+    a.click()
+    window.URL.revokeObjectURL(url) // 浏览器会自动释放createObjectURL创建的对象
+  })
+}

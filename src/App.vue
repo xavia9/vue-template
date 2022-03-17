280a7a71cef08d3e0a06bb5cbba20da9de076fa9
xavia9 <xavia_1@163.com>
2022-03-17 21:20:15 +0800

    src入口文件修改

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,94 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <router-link to="/table">表格组件</router-link>
-    <br />
-    <br />
-    <router-link to="/datetime">时间组件</router-link>
-    <br />
-    <br />
-    <router-link to="/date">日期组件</router-link>
+    <tags-view></tags-view>
   </div>
 </template>
 
 <script>
+// const menuTop = [
+//   {
+//     routePath: '/asyncPage',
+//     name: '异步组件',
+//   },
+//   {
+//     routePath: '/dynamic',
+//     name: '动态组件',
+//   },
+//   {
+//     routePath: '/elementPage/formPage',
+//     name: 'element组件封装',
+//   },
+//   {
+//     routePath: '/echartPage/single-bar',
+//     name: 'echart组件封装',
+//   },
+// ]
+import { getQueryObject } from '../src/common/utils'
+import TagsView from 'components/layout/TagsView.vue'
+
 export default {
-  name: "App",
-  data() {
-    return {};
+  name: 'App',
+  components: {
+    TagsView,
   },
-};
+  data() {
+    return {
+      activeMenuItem: '',
+    }
+  },
+  watch: {
+    '$route.path': {
+      handler(newVal, oldval) {
+        this.activeMenuItem = this.$route.path
+      },
+      deep: true,
+    },
+  },
+  created() {
+    // this.menuTop = menuTop
+    // this.handleMenu()
+    console.log(1)
+    console.log(getQueryObject('http://www.baidu.com?name=1'))
+  },
+  mounted() {
+    // new Promise((resolve, reject) => {
+    //   setTimeout(() => reject(111), 1000)
+    // }).then((data) => {
+    //   console.log(data)
+    // })
+  },
+  methods: {
+    // // 生成菜单
+    // handleMenu() {
+    //   // 生成element侧边栏菜单
+    //   const elementRoute = this.$router.options.routes.find(
+    //     (route) => route.path === '/elementPage'
+    //   )
+    //   const elementMenuList = elementRoute.children
+    //   this.menuList = elementMenuList.map((item) => {
+    //     return {
+    //       routePath: `/elementPage/${item.path}`,
+    //       name: item.meta.name,
+    //       icon: item.meta.icon,
+    //     }
+    //   })
+    // },
+  },
+}
 </script>
 
-<style>
+<style lang="scss">
+html,
+body,
+div,
+p,
+img,
+a {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  height: 100vh;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import './plugins/element.js';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from './common/axios.js'
+import store from './store'
+import directives from './directives'
+// import ElementUI from 'element-ui' //整体引入
+import './plugins/element.js' //按需引入
+// import uIcomponents from './plugins/uIcomponents.js'
+import 'element-ui/lib/theme-chalk/index.css'
 // import './config/rem';
+import './config/index.js'
+// 全局公共样式
+// import './config/other/css.js'
 
-// 全局使用
-Vue.config.productionTip = false;
+// console.log(router)
 
+Vue.config.productionTip = false
+// 全局注册
+Vue.prototype.$request = axios
+// 自定义指令
+Vue.use(directives)
+// Vue.use(ElementUI)
+// Vue.use(uIcomponents)
 //打印App 没有template 但是有render函数 表示vue-template-compiler已经内部将其转换成了render
 // console.log(App);
 
@@ -17,4 +31,4 @@ new Vue({
   store,
   // 直接传入组件对象 App.vue里面的template会替换index.html里面#app的内容
   render: (h) => h(App),
-});
+})
